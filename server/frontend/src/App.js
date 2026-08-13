c4d6a3bd404c8192be1536e8fa94576630aae081
Abdul Razzak <abdulrazzakbhayo4@gmail.com>
2026-08-13 11:02:08 -0400

    Add user registration functionality

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
+
 export default App;
