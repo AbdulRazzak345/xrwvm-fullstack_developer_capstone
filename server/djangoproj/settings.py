9b3aa5ebe4dc91aebcf91d114c20af0b53e30518
AbdulRazzak345 <abdulrazzakbhayo4@gmail.com>
2026-08-18 07:50:25 -0400

    Add current session proxy URL to allowed hosts

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -17,12 +17,14 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'abdulrazzakb-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'abdulrazzakb-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 
 # Allow CSRF requests from the Skills Network proxy
 CSRF_TRUSTED_ORIGINS = [
     'https://abdulrazzakb-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://abdulrazzakb-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 
