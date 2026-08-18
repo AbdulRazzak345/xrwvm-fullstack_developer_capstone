2321a18e7da35c15ce2169f263744b426017058e
abdulrazzak345 <abdulrazzakbhayo4@gmail.com>
2026-08-18 06:57:45 -0400

    Fix homepage template and add About/Contact routes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -2,15 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('djangoapp/', include('djangoapp.urls')),
-
-    path('', TemplateView.as_view(template_name="index.html")),
-
-    path('login/', TemplateView.as_view(template_name="index.html")),
-
-    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('login/', TemplateView.as_view(template_name="Home.html")),
+    path('register/', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ]
